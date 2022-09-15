c7d0689179d5378103635b4e191a0c12c5565601
muneebh3 <muneebhashmi339@gmail.com>
2022-09-14 22:32:47 -0500

    style consistent on different pages

--- switch.js
+++ switch.js
@@ -1,16 +1,18 @@
 function change_style(){
-    var style = localStorage.getItem("color");
-    var sheet = document.getElementById("page_style");
-
-    if (style != "style.css") {
-        sheet.href = "style.css";
-        localStorage.setItem("color", "style.css");
+    if (localStorage.getItem("color") === "style.css") {
+        document.getElementById("page_style").setAttribute("href", "alternate.css");
+        localStorage.setItem("color", "alternate.css");
     }
     else {
-        sheet.href = "style.css";
+        document.getElementById("page_style").setAttribute("href", "style.css");
         localStorage.setItem("color", "style.css");
-
-        sheet.href = "alternate.css";
-        localStorage.setItem("color", "alternate.css");
     } 
+}
+
+window.onload = function() {
+    if (!localStorage.getItem("color")) {
+        localStorage.setItem("color", "style.css");
+    }
+
+    document.getElementById("page_style").setAttribute("href", localStorage.getItem("color"));
 }
